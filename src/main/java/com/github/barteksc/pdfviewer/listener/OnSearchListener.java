286ff6cbf885e57e6aad9899c148f26f77d5b087
DuyLT <fcsteay@gmail.com>
2024-09-18 10:15:37 +0700

    push

---
+++ src/main/java/com/github/barteksc/pdfviewer/listener/OnSearchListener.java
@@ -0,0 +1,9 @@
+package com.github.barteksc.pdfviewer.listener;
+
+/**
+ * Copyright © 2023 Asian Mobile Co.,Ltd
+ * Created by duylt on 21-Feb-23.
+ */
+public interface OnSearchListener {
+    void onSearchComplete();
+}
